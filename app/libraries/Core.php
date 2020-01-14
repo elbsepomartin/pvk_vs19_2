023498112b5f0d9341fa1470e93fd97f30bd3864
Sepo-Martin <sepo-martin.elb@khk.ee>
2020-01-14 13:38:43 +0200

    o

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -10,10 +10,12 @@ class Core
     }
 
     public function getUrl(){
-        $url = $_GET['url'];
-        $url = rtrim($url, '/');
-        $url = filter_var($url, FILTER_SANITIZE_URL);
-        $url = explode('/', $url);
-        return $url;
+        if(isset($_GET['url'])){
+            $url = $_GET['url'];
+            $url = rtrim($url, '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
