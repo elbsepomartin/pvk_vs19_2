6a9391b528e9009df02e8b3341a8b802975ce0b9
Sepo-Martin <sepo-martin.elb@khk.ee>
2020-01-14 13:34:20 +0200

    o

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -6,14 +6,14 @@ class Core
     public function __construct()
     {
         $url = $this->getUrl();
-        echo $url;
+        print_r($url);
     }
 
     public function getUrl(){
         $url = $_GET['url'];
         $url = rtrim($url, '/');
         $url = filter_var($url, FILTER_SANITIZE_URL);
-        $url = explode($url, '/');
+        $url = explode('/', $url);
         return $url;
     }
 }
