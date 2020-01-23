05369bd8f19a7ee4bbf9079a28a3333e0b15ef14
Sepo-Martin <sepo-martin.elb@khk.ee>
2020-01-23 11:43:21 +0200

    o

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -7,4 +7,23 @@ class Database
     private $user = DB_USER;
     private $pass = DB_PASS;
     private $dbname = DB_NAME;
+
+    private $dbh;
+    private $stmt;
+    private $error;
+
+    public function __construct()
+    {
+        $dsn = 'mysql:host='.$this->host.';dbname='.$this->dbname;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+        try{
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+        }   catch (PDOException $e){
+            $this->error = $e->getMessage();
+            echo $this->error.'<br>';
+        }
+    }
 }
