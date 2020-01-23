ebbac3b54e15e32fda906c044556b1e0917f54c3
Sepo-Martin <sepo-martin.elb@khk.ee>
2020-01-23 11:29:58 +0200

    o

--- app/config/constants.php
+++ app/config/constants.php
@@ -5,3 +5,9 @@ define('PROJECT_NAME', 'pvk');
 define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
 // rakenduse juurkataloog
 define('APPROOT', dirname(dirname(__FILE__)));
+
+// db parameetrid
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'pdo');
