cd0065a60c72e8dd79113f30cb5b3040a18caaee
Sepo-Martin <sepo-martin.elb@khk.ee>
2020-01-27 14:24:09 +0200

    o

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,18 +5,7 @@ class Pages extends Controller
 {
     public function __construct()
     {
-        $this->pagesModel = $this->model('Page');
-    }
 
-    public function test(){
-        $usersCount = $this->pagesModel->usersCount();
-        echo '<pre>';
-        print_r($usersCount);
-        echo '</pre>';
-    }
-
-    public function edit(){
-        $this->view('pages/edit');
     }
 
     public function index() {
